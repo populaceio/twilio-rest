0825e23fa54214072f63e0eaa6aee3342f130f17
Dylan Butman <dbutman@gmail.com>
2015-12-30 12:41:10 -0500

    fork

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.vitalreactor/twilio-rest "1.0.0"
+(defproject org.clojars.populaceio/twilio-rest "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Adapter library for the Twilio web service."
   :url "http://github.com/vitalreactor/twilio-rest"
   :scm "http://github.com/vitalreactor/twilio-rest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
                  [cheshire "5.3.1"]
                  [clj-http "2.0.0"]
                  [midje    "1.8.2"]]
